917f83fff2a7e5313adc7055a1fe5054e1900a30
Klaudeta Mertiku <klaudeta.mertiku@reaktor.com>
2022-02-17 12:18:47 +0200

    filtering by package name

--- components/PackageList.tsx
+++ components/PackageList.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useState } from "react";
 import sharedClasses from "./Package.module.css";
 import classes from "./PackageList.module.css";
 
@@ -8,14 +9,21 @@ type PackageListProps = {
 }
 
 const PackageList: React.FC<PackageListProps> = (props) => {
+    const [packageList, setPackageList] = useState(props.items as { name: string, version: string }[]);
+    
+    const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+        const newValue = e.currentTarget.value;
+        setPackageList(props.items.filter(item => item.name.includes(newValue)));
+    }
+    
     return (
         <div>
             <header className={sharedClasses.header}>
                 <div className={sharedClasses.logo}>Package Viewer</div>
-                <input type="text" placeholder="Search packages" className={classes.search}></input>
+                <input type="text" placeholder="Search packages" className={classes.search} onChange= {onChange}></input>
             </header>
             <ul className={classes.packagesul}>
-                {props.items.map(item => (
+                {packageList.map(item => (
                     <Link href={`/${item.name}`} key={item.name} >
                         <li className={classes.packageli}>
                             <span>{item.name}</span>
