e8b023fe7f38e948c5a9512f810d0e2cf7178c10
Klaudeta Mertiku <klaudeta.mertiku@reaktor.com>
2022-02-17 23:05:20 +0200

    anchor tag inside Link

--- components/PackageList.tsx
+++ components/PackageList.tsx
@@ -10,25 +10,27 @@ type PackageListProps = {
 
 const PackageList: React.FC<PackageListProps> = (props) => {
     const [packageList, setPackageList] = useState(props.items as { name: string, version: string }[]);
-    
+
     const onChange = (e: React.FormEvent<HTMLInputElement>) => {
         const newValue = e.currentTarget.value;
         setPackageList(props.items.filter(item => item.name.includes(newValue)));
     }
-    
+
     return (
         <div>
             <header className={sharedClasses.header}>
                 <div className={sharedClasses.logo}>Package Viewer</div>
-                <input type="text" placeholder="Search packages" className={classes.search} onChange= {onChange}></input>
+                <input type="text" placeholder="Search packages" className={classes.search} onChange={onChange}></input>
             </header>
             <ul className={classes.packagesul}>
                 {packageList.map(item => (
                     <Link href={`/${item.name}`} key={item.name} >
-                        <li className={classes.packageli}>
-                            <span>{item.name}</span>
-                            <span>{item.version}</span>
-                        </li>
+                        <a>
+                            <li className={classes.packageli}>
+                                <span>{item.name}</span>
+                                <span>{item.version}</span>
+                            </li>
+                        </a>
                     </Link>
                 ))}
             </ul>
