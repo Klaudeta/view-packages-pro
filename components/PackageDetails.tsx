3cbac86abe20f921578aabe5da980b6a4fe62cb9
Klaudeta Mertiku <klaudeta.mertiku@reaktor.com>
2022-02-21 09:48:29 +0200

    setting Prettier as the default formatter

--- components/PackageDetails.tsx
+++ components/PackageDetails.tsx
@@ -3,53 +3,51 @@ import sharedClasses from "./Package.module.css";
 import classes from "./PackageDetails.module.css";
 
 type PackageDetailsProp = {
-    name: string,
-    version: string,
-    description?: string,
-    depends?: string[],
-    dependant?: string[]
-}
+  name: string;
+  version: string;
+  description?: string;
+  depends?: string[];
+  dependant?: string[];
+};
 
 const PackageDetails: React.FC<PackageDetailsProp> = (props) => {
-    return (
-        <div>
-            <header className={sharedClasses.header}>
-                <div className={sharedClasses.logo}>Package Details</div>
-                <Link href='/' >
-                    <a>
-                        <span className={classes.back}>
-                            Back to package list
-                        </span>
-                    </a>
-                </Link>
-            </header>
+  return (
+    <div>
+      <header className={sharedClasses.header}>
+        <div className={sharedClasses.logo}>Package Details</div>
+        <Link href="/">
+          <a>
+            <span className={classes.back}>Back to package list</span>
+          </a>
+        </Link>
+      </header>
 
-            <table className={classes.details}>
-                <tbody>
-                    <tr>
-                        <td>Name</td>
-                        <td>{props.name}</td>
-                    </tr>
-                    <tr>
-                        <td>Version</td>
-                        <td>{props.version}</td>
-                    </tr>
-                    <tr>
-                        <td>Description</td>
-                        <td>{props.description}</td>
-                    </tr>
-                    <tr>
-                        <td>Depends</td>
-                        <td>{props.depends?.join(', ')}</td>
-                    </tr>
-                    <tr>
-                        <td>Dependent</td>
-                        <td>{props.dependant?.join(', ')}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    );
-}
+      <table className={classes.details}>
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>{props.name}</td>
+          </tr>
+          <tr>
+            <td>Version</td>
+            <td>{props.version}</td>
+          </tr>
+          <tr>
+            <td>Description</td>
+            <td>{props.description}</td>
+          </tr>
+          <tr>
+            <td>Depends</td>
+            <td>{props.depends?.join(", ")}</td>
+          </tr>
+          <tr>
+            <td>Dependent</td>
+            <td>{props.dependant?.join(", ")}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default PackageDetails;
--- components/PackageList.tsx
+++ components/PackageList.tsx
@@ -3,38 +3,43 @@ import { useState } from "react";
 import sharedClasses from "./Package.module.css";
 import classes from "./PackageList.module.css";
 
-
 type PackageListProps = {
-    items: { name: string, version: string }[]
-}
+  items: { name: string; version: string }[];
+};
 
 const PackageList: React.FC<PackageListProps> = ({ items }) => {
-    const [packageList, setPackageList] = useState(items);
+  const [packageList, setPackageList] = useState(items);
 
-    const onChange = (e: React.FormEvent<HTMLInputElement>) =>
-        setPackageList(items.filter(item => item.name.includes(e.currentTarget.value)))
+  const onChange = (e: React.FormEvent<HTMLInputElement>) =>
+    setPackageList(
+      items.filter((item) => item.name.includes(e.currentTarget.value))
+    );
 
-    return (
-        <div>
-            <header className={sharedClasses.header}>
-                <div className={sharedClasses.logo}>Package Viewer</div>
-                <input type="text" placeholder="Search packages" className={classes.search} onChange={onChange}></input>
-            </header>
-            <ul className={classes.packagesul}>
-                {packageList.map(item => (
-                    <Link href={`/${item.name}`} key={item.name} passHref>
-                        <a>
-                            <li className={classes.packageli}>
-                                <span>{item.name}</span>
-                                <span>{item.version}</span>
-                            </li>
-                        </a>
-                    </Link>
-                ))}
-            </ul>
-        </div>
-
-    )
-}
+  return (
+    <div>
+      <header className={sharedClasses.header}>
+        <div className={sharedClasses.logo}>Package Viewer</div>
+        <input
+          type="text"
+          placeholder="Search packages"
+          className={classes.search}
+          onChange={onChange}
+        ></input>
+      </header>
+      <ul className={classes.packagesul}>
+        {packageList.map((item) => (
+          <Link href={`/${item.name}`} key={item.name} passHref>
+            <a>
+              <li className={classes.packageli}>
+                <span>{item.name}</span>
+                <span>{item.version}</span>
+              </li>
+            </a>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default PackageList;
