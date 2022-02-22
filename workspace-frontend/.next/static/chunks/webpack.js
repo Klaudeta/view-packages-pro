9bb8e87b46bb402a26e242b95626cc6068b212ab
Klaudeta Mertiku <klaudeta.mertiku@reaktor.com>
2022-02-22 15:50:00 +0200

    porper git ignoring .next

--- workspace-frontend/.next/static/chunks/webpack.js
+++ workspace-frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d02df7151970d35d"; }
+/******/ 		__webpack_require__.h = function() { return "e8320b310dfdb07f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- workspace-frontend/.next/static/webpack/webpack.29898952bc236c80.hot-update.js
+++ workspace-frontend/.next/static/webpack/webpack.29898952bc236c80.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "52eb5f5db3a1e945"; }
+/******/ 	__webpack_require__.h = function() { return "de060c82c4492afa"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
