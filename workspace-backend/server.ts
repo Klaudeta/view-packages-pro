705beb5ddc242a078109154e684801ddbc370fa1
Klaudeta Mertiku <klaudeta.mertiku@reaktor.com>
2022-02-23 12:51:24 +0200

    using process.env.PORT

--- workspace-backend/server.ts
+++ workspace-backend/server.ts
@@ -34,4 +34,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: err.message });
 });
 
-app.listen(3001);
+app.listen(process.env.PORT || 3001);
